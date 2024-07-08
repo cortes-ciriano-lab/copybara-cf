ecbac70b85c403b7cb5355e6e1fbed601d0bebf0
Carolin Sauer <carolinm.sauer@gmail.com>
2024-07-08 09:54:01 +0100

    add paraeters and options to ACN fitting script.

--- COPYBARA/fit_absolute_CN.py
+++ COPYBARA/fit_absolute_CN.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.stats import gaussian_kde
 import statistics
 import sys
+import argparse
 # import math
 # from scipy.stats import skew as scipy_skew
 # from scipy.stats import kurtosis as scipy_kurtosis
@@ -13,184 +14,29 @@ import CN_helper as cnh
 #----
 # 1. Parse command line arguments
 #----
+parser = argparse.ArgumentParser(description="Estimate purity and ploidy and fit absolute copy number.")
+parser.add_argument('-i', '--input', type=str, help='Path to segmented log2r copy number tsv file.', required=True)
+parser.add_argument('-a', '--allele_counts', type=str, help='Path to hetSNPs allele counts to estimate purity.', required=True)
+parser.add_argument('-s', '--sample_prefix', default='bam', type=str, help='Sample name of prefix to use for out files.', required=False)
+parser.add_argument('-o', '--out_dir', type=str, default='.', help='Path to out directory. Default is working directory', required=False)
+args = parser.parse_args()
 
+###############
+# ADD IN FITTING PARAMETERS... i.e. PS parameters for purity and ploidy estimation parameters...
+###############
 
-#----
-# 2. Define functions
-#----
-# def is_constant(x):
-#     return np.all(x == x[0])
-
-# def Modes(x, min_size=0.1):
-#     '''
-#     The Modes function is a simple, deterministic function that differences the kernel density of x and reports a number of modes equal to half the number of changes in direction, 
-#     although the min.size function can be used to reduce the number of modes returned, and defaults to 0.1, eliminating modes that do not have at least 10% of the distributional area. 
-#     The Modes function returns a list with three components: modes, modes.dens, and size. 
-#     The elements in each component are ordered according to the decreasing density of the modes. The modes component is a vector of the values of x associated with the modes. 
-#     The modes.dens component is a vector of the kernel density estimates at the modes. 
-#     The size component is a vector of the proportion of area underneath each mode. 
-#     Function adapted from the Modes() function of the "LaplacesDemon" R package
-#     '''
-#     if x is None:
-#         raise ValueError("The x argument is required.")
-#     # Convert input to a numpy array of floats and filter out non-finite values
-#     x = np.asarray(x).astype(float)
-#     x = x[np.isfinite(x)]
-#     # Check if the input array is constant
-#     if is_constant(x):
-#         return {'modes': [np.nan], 'mode_dens': [np.nan], 'size': [1]}
-#     # Compute the kernel density estimate
-#     density = gaussian_kde(x)
-#     # Create a grid of 1000 points over the range of the data
-#     x_grid = np.linspace(np.min(x), np.max(x), 1000)
-#     # Evaluate the density over the grid
-#     dens_y = density(x_grid)
-#     # Compute the differences between consecutive density values
-#     dens_y_diff = np.diff(dens_y)
-#     # Identify where the density is increasing (1) or not increasing (0)
-#     incr = np.where(dens_y_diff > 0, 1, 0)
-#     # Initialize the list of segment boundaries
-#     begin = [0]
-#     for i in range(1, len(incr)):
-#         if incr[i] != incr[i - 1]:
-#             begin.append(i)
-#     # Add the end of the array as the final boundary
-#     begin.append(len(incr))
-#     #
-#     size = []
-#     modes = []
-#     mode_dens = []
-#     # Sum of all density values for normalization
-#     dens_y_sum = np.sum(dens_y)
-#     #
-#     j = 0
-#     while j < len(begin) - 1:
-#         start_idx = begin[j] # Define the start and end of the current segment
-#         end_idx = begin[j + 2] if j + 2 < len(begin) else len(dens_y) # Extract the segment of the density
-#         segment = dens_y[start_idx:end_idx]  # Calculate the size of the segment
-#         segment_size = np.sum(segment) / dens_y_sum
-#         if segment_size >= min_size: # Define the grid points and density values for the segment
-#             kde_x = x_grid[start_idx:end_idx]
-#             kde_y = segment
-#             mode_idx = np.argmax(kde_y) # Find the index of the mode (maximum density) within the segment
-#             modes.append(kde_x[mode_idx]) # Store the mode and its density
-#             mode_dens.append(kde_y[mode_idx])
-#             size.append(segment_size) # Store the size of the segment
-#         j += 2
-#     # Order the results by density in descending order
-#     order = np.argsort(mode_dens)[::-1]
-#     size = np.array(size)[order]
-#     modes = np.array(modes)[order]
-#     mode_dens = np.array(mode_dens)[order]
-#     # Normalize the sizes to sum to 1 if their sum exceeds 1
-#     if np.sum(size) > 1:
-#         size = size / np.sum(size)
-#     # Return dictionary
-#     return {'modes': modes, 'mode_dens': mode_dens, 'size': size}
-
-# def is_unimodal(x, min_size=0.1):
-#     '''
-#     Function to test if data is unimodal
-#     '''
-#     modes_result = Modes(x, min_size)
-#     if len(modes_result['modes']) == 1 and not np.any(np.isnan(modes_result['modes'])):
-#         return True
-#     else:
-#         return False
-
-# def skew(x, na_rm=True, type=3):
-#     '''
-#     Calculate the skewness of the input array x based on the specified type.
-#     '''
-#     if na_rm:
-#         x = x[~np.isnan(x)]
-#     n = len(x)
-#     if n == 0:
-#         return np.nan
-#     #
-#     if type == 1:
-#         skewer = np.sqrt(n) * (np.sum((x - np.mean(x))**3) / (np.sum((x - np.mean(x))**2)**(3/2)))
-#     elif type == 2:
-#         skewer = n * np.sqrt(n - 1) * (np.sum((x - np.mean(x))**3) / ((n - 2) * np.sum((x - np.mean(x))**2)**(3/2)))
-#     elif type == 3:
-#         skewer = np.sum((x - np.mean(x))**3) / (n * np.std(x)**3)
-#     else:
-#         raise ValueError("Type must be 1, 2, or 3")
-#     #
-#     return skewer
-
-# def kurtosi(x, na_rm=True, type=3):
-#     '''
-#     Calculate the kurtosis of the input array x based on the specified type.
-#     '''
-#     if na_rm:
-#         x = x[~np.isnan(x)]
-#     n = len(x)
-#     if n == 0:
-#         return np.nan
-#     #
-#     if type == 1:
-#         kurt = (np.sum((x - np.mean(x))**4) * n) / (np.sum((x - np.mean(x))**2)**2) - 3
-#     elif type == 2:
-#         kurt = (n * (n + 1) * np.sum((x - np.mean(x))**4)) / ((n - 1) * (n - 2) * (n - 3) * (np.sum((x - np.mean(x))**2) / (n - 1))**2) - 3 * (n - 1)**2 / ((n - 2) * (n - 3))
-#     elif type == 3:
-#         kurt = (np.sum((x - np.mean(x))**4) / (n * np.std(x)**4)) - 3
-#     else:
-#         raise ValueError("Type must be 1, 2, or 3")
-#     #
-#     return kurt
-
-
-# def bimodality_coefficient(x, na_rm=False):
-#     '''
-#     Calculate the bimodality coefficient using the skew and kurtosi functions.
-#     '''
-#     if na_rm:
-#         x = x[~np.isnan(x)]
-#     n = len(x)
-#     if n == 0:
-#         return np.nan
-#     #
-#     m3 = skew(x, na_rm=na_rm, type=2)
-#     # m3 = scipy_skew(x)
-#     m4 = kurtosi(x, na_rm=na_rm, type=2)
-#     # m4 = scipy_kurtosis(x)
-#     bc = (m3**2 + 1) / (m4 + 3 * ((n - 1)**2 / ((n - 2) * (n - 3))))
-#     return bc
-
-
-# def mode(x):
-#     if x is None: 
-#         print("The x argument in Modes function is required.")
-#         return None
-#     npx = np.asarray(x)[np.isfinite(np.asarray(x))]
-#     if is_constant(npx):
-#         return np.nan
-#     if np.all(npx == np.round(npx)):
-#         values, counts = np.unique(npx, return_counts=True)
-#         mode_value = values[np.argmax(counts)]
-#     else:
-#         kde = gaussian_kde(npx)
-#         kde_values = kde.evaluate(npx)
-#         mode_value = x[np.argmax(kde_values)]
-#     return mode_value
-
+input_file = args.input
+allele_counts_file = args.allele_counts
+prefix = args.sample_prefix
+outdir = args.out_dir
 
 #----
-# 3. Estimate purity using phased heterozygous SNPs
+# 2. Estimate purity using phased heterozygous SNPs
+### Method/principles based on: https://doi.org/10.1371/journal.pone.0045835
 #----
-# Method/principles based on: https://doi.org/10.1371/journal.pone.0045835
-# allele_counts_path = "chr17_test/chr17_215000645_allele_counts_hetSNPs.tsv"
-# allele_counts_path = "215000645_wg_allele_counts.txt"
-# allele_counts_path = "test_out_20240628/215000645_allele_counts_hetSNPs.tsv"
-allele_counts_path = "../test_purity_estimator/ac_pur_test_out_20240630/sarcoma/215000173_ac_purity/215000173_allele_counts_hetSNPs.tsv"
-
-# process input - allele_counts_hetSNPs.tsv
-
-allele_counts_path=sys.argv[1]
 
 allele_counts = []
-with open(allele_counts_path, "r") as file:
+with open(allele_counts_file, "r") as file:
     for line in file:
         fields = line.strip().split("\t")
         A,C,G,T,N = int(fields[5]),int(fields[6]),int(fields[7]),int(fields[8]),int(fields[9])
@@ -206,19 +52,13 @@ with open(allele_counts_path, "r") as file:
 
 ac_mean_depth = statistics.mean([x[-1] for x in allele_counts])
 dp_cutoff = 2 * ac_mean_depth # to exclude potentially amplified/gained regions as those will impact BAF distribution
-# dp_cutoff = 100
 no_hetSNPs = len(allele_counts)
-
 
 ### PARAM to add in!!! ###
 min_PS_size = 10
 
 # get unique phase sets to iterate (or multiprocess through)
 phasesets = list(dict.fromkeys([x[-2] for x in allele_counts]))
-# phasesets_dict = dict.fromkeys([x[-2] for x in allele_counts])
-
-# TESTING
-# phasesets = [phasesets[0]]
 
 phasesets_dict = {}
 ps_summary = []
@@ -240,10 +80,10 @@ for ps in phasesets:
             # all AFs at all het SNP position within ps
             af = [float(x[10]) for x in ac_ps] + [float(x[11]) for x in ac_ps]
             # check if distribution is not unimodal and estimate bimodality coefficient
-            # if cnh.is_unimodal(af) == False:
-            #     ps_bc = cnh.bimodality_coefficient(af)
-            if is_unimodal(af) == False:
-                ps_bc = bimodality_coefficient(af)
+            if cnh.is_unimodal(af) == False:
+                ps_bc = cnh.bimodality_coefficient(af)
+            # if is_unimodal(af) == False:
+                # ps_bc = bimodality_coefficient(af)
                 # ps_bc_weighted = ps_weight * ps_bc
                 if ps_depth < dp_cutoff:
                     phasesets_dict[ps] = ac_ps
@@ -261,9 +101,6 @@ ps_summary = sorted(ps_summary,key=lambda x: x[-1]) # sort by ps_bc
 ps_summary_long = [x for x in ps_summary if x[2] > 500000]
 ps_top = [x[0] for x in ps_summary_long[-10:]]
 
-# ps_top = [x[0] for x in ps_summary[-10:]]
-
-
 # pull out data for ps_top
 ps_top_acs = []
 for pst in ps_top:
@@ -278,13 +115,20 @@ purity = statistics.mean([1-(1-max(pur0,pur1))*2]+[1-(min(pur0,pur1))*2])
 print(purity)
 
 
-    # add weighting of bc per PS for selection
-    # ALSO add if statement on Depth < 100?? and other filters? Write dictionary or list with PS summary and also output data for purity estimation.. 
+#----
+# 2. Estimate ploidy and fit ACN using estimated sample purity
+### Method/principles based on rascal R package: https://www.biorxiv.org/content/10.1101/2021.07.19.452658v1
+#----
 
 
 
 
 
+
+
+
+
+############################################################################################################################
 # filter snps with DP<20
 # get mean DP for each phasing set
 # remove SNPs with AF_0 or AF_1 = 1 or = 0
